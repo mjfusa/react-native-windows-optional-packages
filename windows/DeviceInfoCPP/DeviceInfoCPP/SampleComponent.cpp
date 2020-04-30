db84522cb2e8b01fa0f0e0c200933571ebcbd41d
Mike Francis <mike.francis@live.com>
2020-04-30 11:55:13 -0700

    vanilla cpp wrc DeviceInfoCPP

---
+++ windows/DeviceInfoCPP/DeviceInfoCPP/SampleComponent.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "SampleComponent.h"
+#include "SampleComponent.g.cpp"
+#include <winrt/Windows.Security.ExchangeActiveSyncProvisioning.h>
+
+using namespace winrt::Windows::Security::ExchangeActiveSyncProvisioning;
+
+namespace winrt::DeviceInfoCPP::implementation
+{
+    winrt::hstring SampleComponent::DeviceInfo()
+    {
+        auto info = EasClientDeviceInformation();
+        return info.SystemProductName();
+    }
+
+    void SampleComponent::DeviceInfo(winrt::hstring /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ windows/DeviceInfoCPP/DeviceInfoCPP/SampleComponent.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "SampleComponent.g.h"
+
+namespace winrt::DeviceInfoCPP::implementation
+{
+    struct SampleComponent : SampleComponentT<SampleComponent>
+    {
+        SampleComponent() = default;
+
+        winrt::hstring DeviceInfo();
+        void DeviceInfo(winrt::hstring value);
+    };
+}
+
+namespace winrt::DeviceInfoCPP::factory_implementation
+{
+    struct SampleComponent : SampleComponentT<SampleComponent, implementation::SampleComponent>
+    {
+    };
+}
